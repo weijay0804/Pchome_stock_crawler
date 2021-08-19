fb35c31cb4ac2929c08626c2899b0ae2090f6a0e
Jay <74105438+weijay0804@users.noreply.github.com>
2021-08-19 19:52:38 +0800

    爬取 資產負債資料

--- file_function.py
+++ file_function.py
@@ -4,12 +4,31 @@
 
 '''
 
-def save_datas(datas : list, filename : str) -> None:
+import os
+
+
+def save_datas(datas : list, dirname : str, filename : str) -> None:
     ''' 儲存資料 '''
 
-    with open(f'{filename}.txt', 'w', encoding='utf-8') as f:
+    if not os.path.exists(f'./DATA/{dirname}'):
+        os.mkdir(f'./DATA/{dirname}')
+
+    with open(f'DATA/{dirname}/{filename}.txt', 'w', encoding='utf-8') as f:
         for data in datas:
             f.write(str(data))
             f.write('\n')
 
     return None
+
+def read_datas(dirname : str, filename : str) -> list:
+    ''' 讀取資料 '''
+
+    if not os.path.exists(f'DATA/{dirname}/{filename}.txt'):
+        return '資料不存在'
+
+    datas_list = []
+    with open(f'DATA/{dirname}/{filename}.txt', 'r', encoding='utf-8') as f:
+        data = f.read()
+        datas_list.append(data)
+
+    return datas_list
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from crawler_function import get_stock_numbers, get_balance_sheet
-from file_function import save_datas
-
-data = get_balance_sheet('https://pchome.megatime.com.tw/stock/sto2/ock0/sid2303.html')
-
-save_datas(data, 'balance_sheet')
-
-print(data)
+from file_function import save_datas, read_datas
+import time
 
 
+number_list = read_datas('number', 'stock_number')
 
-    
+number_list = number_list[0].split('\n') 
+
+for number in number_list:
+    sheet = get_balance_sheet(f'https://pchome.megatime.com.tw/stock/sto2/ock0/sid{number}.html')
+    save_datas(sheet, str(number), 'balance_sheet')
+    print(sheet)
+    time.sleep(2)
