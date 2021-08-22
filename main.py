725e911d0cfa102fdc5dc66b15293502acbd4d28
Jay <74105438+weijay0804@users.noreply.github.com>
2021-08-22 22:37:14 +0800

    重構程式碼

--- main.py
+++ main.py
@@ -1,99 +1,48 @@
-from crawler_function import get_stock_numbers, get_balance_sheet, get_income_statement, send_request
-from file_function import save_datas, read_datas
-import openpyxl
 import time
-import json
-import os
+
+# --- 自訂函式 ---
+from crawler_function import get_balance_sheet, get_income_statement
+from file_function import save_datas, read_datas
+from export_to_excel import export_income_to_excel
 
 
-def main():
+
+
+def income(seasion : str) -> None:
+    ''' 取得 損益表資料 並存成 json 文件 (seasion = 20212 or 20211) '''
+
     number_list = read_datas('number', 'stock_number.txt')
 
     number_list = number_list[0].split('\n')
 
     for number in number_list:
-        income_statement_20211 = get_income_statement(f'https://pchome.megatime.com.tw/stock/sto2/ock1/20211/sid{number}.html')
-        print(income_statement_20211)
+
+        datas_list = get_income_statement(f'https://pchome.megatime.com.tw/stock/sto2/ock1/{seasion}/sid{number}.html')
+        print(datas_list)
         print('-----------------------')
-        save_datas(income_statement_20211, str(number), 'income_statement_20211.json')
-        time.sleep(2)
+        save_datas(datas_list, str(number), f'income_statement_{seasion}.json')
+        time.sleep(5)
 
     print('Done!')
 
-def export_balance_to_excel():
-    ''' 將資產負債表資料整理成 excel 型式 並匯出 '''
-    
-    number_list = read_datas('number', 'stock_number.txt')  # 讀取公司股票代號
 
-    for number in number_list[0].split('\n'):
-        workbook = openpyxl.Workbook()  # 建立活頁簿
-        workbook.create_sheet('第二季') # 建立第二個工作表
+def balance() -> None:
+    ''' 取得 資產負債表 並存成 json 文件 '''
 
-        sheet1 = workbook.worksheets[0] # 取得第一個工作表
-        sheet1.title = '第一季' # 更改工作表名稱
+    number_list = read_datas('number', 'stock_number.txt')
 
-        sheet2 = workbook.worksheets[1] # 取得第二個工作表
+    number_list = number_list[0].split('\n')
 
-        
+    for number in number_list:
+        datas_list = get_balance_sheet(f'https://pchome.megatime.com.tw/stock/sto2/ock0/sid{number}.html')
+        print(datas_list)
+        print('-----------------------')
+        save_datas(datas_list, str(number), 'balance_sheet.json')
+        time.sleep(5)
 
-        # 取得資產負債表資訊
-        datas = read_datas(str(number), 'balance_sheet.json')
-        
-        s1_data_list = []   # 儲存 第一季 資料 list
-        s2_data_list = []   # 儲存 第二季 資料 list
-
-        for data in datas:
-            items = list(data.items())[0]   # 將字典轉為 list
-
-            # 將 str 型態轉為 float (123,456,1.12 -> 1234561.12)
-            try:
-                s1_values = float(items[1][0].replace(',',''))
-                s1_percent = float(items[1][1].replace(',',''))
-
-                s2_values = float(items[1][2].replace(',', ''))
-                s2_percent = float(items[1][3].replace(',', ''))
-
-            except:
-                s1_values = 0
-                s1_percent = 0
-
-                s2_values = 0
-                s2_percent = 0
-
-            s1_values_list = [
-                items[0],
-                s1_values,
-            ]
-
-            s1_percent_list = [
-                '%',
-                s1_percent
-            ]
-
-            s2_values_list = [
-                items[0],
-                s2_values
-            ]
-
-            s2_percent_list = [
-                '%',
-                s2_percent
-            ]
-            
-            s1_data_list.append(s1_values_list)
-            s1_data_list.append(s1_percent_list)
-
-            s2_data_list.append(s2_values_list)
-            s2_data_list.append(s2_percent_list)
-        
-        for i in s1_data_list:
-            sheet1.append(i)
-
-        for j in s2_data_list:
-            sheet2.append(j)
-
-        workbook.save(f'DATA/{number}/{number}_balance.xlsx')
+    print('Done!')
 
 
 if __name__ == '__main__':
-    export_balance_to_excel()
+    export_income_to_excel('20211')
+        
